45a31c47d6ecfcbda08a9afc2ac4ee37f9f08e70
nicole-fabian <102332600+nicolefabian@users.noreply.github.com>
2025-01-20 22:01:51 +1300

    data-driven automation

--- function.js
+++ function.js
@@ -1,17 +1,38 @@
 //This is where all the functions are defined
 var { By } = require("selenium-webdriver");
-const chai = require('chai').should();
+const chai = require("chai").should();
+var fs = require("fs"); //file system module
 
+//read data from the json
+//match the key for testname
+var readData = async function (tcName) {
+  const content = fs.readFileSync("./data.json");
+  const obj = JSON.parse(content); //holding all the json data
+  //loop through the json data
+  for (let index = 0; index < obj.length; index++) {
+    if (tcName == obj[index]["testName"]) {
+      return obj[index];
+    }
+  }
+};
+// var myData = readData("tc002"); //returns the data for tc001 because it matches the if "testName"
+// console.log(myData);
 
 var login = async function (driver, username, password) {
+  globalData = await readData("global");
+  console.log(`Going to ${globalData.data.url}`);
   // 1. Navigate to the login page.
-  await driver.get("https://bookstore.qacurry.com");
+  await driver.get(globalData.data.url);
   // 2. Enter valid username and password.
-  await driver.findElement(By.xpath("//input[@name='login_email']")).sendKeys(username);
-  await driver.findElement(By.xpath("//input[@name='login_password']")).sendKeys(password);
+  await driver
+    .findElement(By.xpath("//input[@name='login_email']"))
+    .sendKeys(username);
+  await driver
+    .findElement(By.xpath("//input[@name='login_password']"))
+    .sendKeys(password);
   //3.  Click on the login button.
-  await driver.findElement(By.xpath("//input[@value='Login']")).click()
-}
+  await driver.findElement(By.xpath("//input[@value='Login']")).click();
+};
 
 var sanityCheckPageLoad = async function (driver, expectedText) {
   //verify the text -> Hello, Student
@@ -22,11 +43,16 @@ var sanityCheckPageLoad = async function (driver, expectedText) {
 };
 
 var sanityCheckInvalidLogin = async function (driver, expectedText) {
-  var actualText = await driver.findElement(By.xpath("//h3[normalize-space()='Your Login Name or Password is invalid']")).getText(); 
+  var actualText = await driver
+    .findElement(
+      By.xpath(
+        "//h3[normalize-space()='Your Login Name or Password is invalid']"
+      )
+    )
+    .getText();
   actualText.should.equal(expectedText);
 };
 
 module.exports = {
-    login, sanityCheckPageLoad, sanityCheckInvalidLogin
+    login, sanityCheckPageLoad, sanityCheckInvalidLogin, readData
 }
-
--- index.js
+++ index.js
@@ -36,20 +36,82 @@
 // tc001();
 
 //TC001 -> HAPPY PATH   
+// const exp = require('constants');
+// const {Builder, Browser, By} = require('selenium-webdriver'); //node .\index.js to run the script
+// var {login, sanityCheckPageLoad,sanityCheckInvalidLogin} = require('./function'); //importing the login function from function.js
+
+// var tc001 = async function() {
+//     //initiate the browser
+//     let driver = await new Builder().forBrowser(Browser.FIREFOX).build();
+    
+//     try {
+//         //login function -> pass driver
+//         console.log("TC001:Login started");
+//         await login(driver,"student@qacurry.com","Q@curry");
+//         //verify the text -> Hello, Student
+//         await sanityCheckPageLoad(driver, "Hello Student");
+//         //verify the login
+//        console.log("TC001:Login successful");
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }   
+//     finally {
+//         await driver.quit();
+//         //
+//         console.log("TC001:Test case execution completed");
+//     }
+// }
+// //execute the test case
+// //tc001();
+
+
+// var tc002 = async function() {
+//     //initiate the browser
+//     let driver = await new Builder().forBrowser(Browser.FIREFOX).build();
+
+//     try {
+//         //login function -> pass driver
+//         console.log("TC002:Login started");
+//         await login(driver,"not-student@qacurry.com","not-Q@curry");
+//         await sanityCheckInvalidLogin(driver, "Your Login Name or Password is invalid");
+//         //verify the login
+//          console.log("TC002:Login successful");
+//     }
+//     catch (err) {
+//         console.log(err); 
+//     }   
+//     finally {
+//         await driver.quit();
+//         //
+//         console.log("TC002:Test case execution completed");
+//     }
+    
+// }
+// //tc002();
+
+// //executing multiple test cases without mocha
+// var testsuite = async function() {
+//     await tc001();
+//     await tc002();
+// }();
+
+
+
 const exp = require('constants');
 const {Builder, Browser, By} = require('selenium-webdriver'); //node .\index.js to run the script
-var {login, sanityCheckPageLoad,sanityCheckInvalidLogin} = require('./function'); //importing the login function from function.js
+var {login, sanityCheckPageLoad,sanityCheckInvalidLogin, readData} = require('./function'); //importing the login function from function.js
 
 var tc001 = async function() {
     //initiate the browser
     let driver = await new Builder().forBrowser(Browser.FIREFOX).build();
-
+    tcData = await readData("tc001"); //returns the data for tc001 
     try {
         //login function -> pass driver
         console.log("TC001:Login started");
-        await login(driver,"student@qacurry.com","Q@curry");
+        await login(driver,tcData.data.username,tcData.data.password);
         //verify the text -> Hello, Student
-        await sanityCheckPageLoad(driver, "Hello Student");
+        await sanityCheckPageLoad(driver, tcData.data.expectedText);
         //verify the login
        console.log("TC001:Login successful");
     }
@@ -69,12 +131,12 @@ var tc001 = async function() {
 var tc002 = async function() {
     //initiate the browser
     let driver = await new Builder().forBrowser(Browser.FIREFOX).build();
-
+    tcData = await readData("tc002"); //returns the data for tc001 
     try {
         //login function -> pass driver
         console.log("TC002:Login started");
-        await login(driver,"not-student@qacurry.com","not-Q@curry");
-        await sanityCheckInvalidLogin(driver, "Your Login Name or Password is invalid");
+        await login(driver,tcData.data.username,tcData.data.password);
+        await sanityCheckInvalidLogin(driver, tcData.data.expectedText);
         //verify the login
          console.log("TC002:Login successful");
     }
